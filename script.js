0b48e6c395ca41442d9fb1dc87b6614cc74f931e
LucianiChristian <luciani_christian@yahoo.com>
2022-05-12 16:12:33 -0500

    Grid Is Hardcoded To Draw Exclusively In Black

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let colorMode = 'black';
+
+
 function generateColumns(gridSideLength) {
     const gridContainer = document.querySelector('.grid-container');
     let gridColumn;
@@ -9,7 +12,8 @@ function generateColumns(gridSideLength) {
         gridColumn.classList.add('grid-column');
         gridContainer.appendChild(gridColumn);
         for(let j = 0; j < gridSideLength; j++) {
-            gridSquare = document.createElement('div');      
+            gridSquare = document.createElement('div');
+            gridSquare.setAttribute('onmouseover', 'drawColor(this)');      
             gridColumn.appendChild(gridSquare);
         }
     }
@@ -18,4 +22,13 @@ function generateColumns(gridSideLength) {
 function clearGrid() {
     const gridContainer = document.querySelector('.grid-container');
     gridContainer.innerHTML = "";
+}
+
+function setColorMode(newMode) {
+    colorMode = newMode;
+    console.log(colorMode);
+}
+
+function drawColor(gridSquare) {
+    gridSquare.style.background = 'black';
 }
