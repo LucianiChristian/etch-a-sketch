baf9e808842a901b302501877fc40ca79035615d
LucianiChristian <luciani_christian@yahoo.com>
2022-05-12 13:32:35 -0500

    Grid Density Buttons Fully Functional

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
-function generateColumns(gridWidth) {
+function generateColumns(gridSideLength) {
     const gridContainer = document.querySelector('.grid-container');
     let gridColumn;
 
     gridContainer.innerHTML = "";
 
-    for(let i = 0; i < gridWidth; i++) {
+    for(let i = 0; i < gridSideLength; i++) {
         gridColumn = document.createElement('div');
+        gridColumn.classList.add('grid-column');
         gridContainer.appendChild(gridColumn);
+        for(let j = 0; j < gridSideLength; j++) {
+            gridSquare = document.createElement('div');      
+            gridColumn.appendChild(gridSquare);
+        }
     }
 }
